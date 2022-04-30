16a61da0565732e829fecdc845aa797b077bb8a1
Arima <tjendung97pain@gmail.com>
2022-04-30 22:25:48 +0700

    add all coffee shop file

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+let menu = document.querySelector("#menu-btn");
+let navbar = document.querySelector(".header .flex .navbar");
+
+menu.onclick = () => {
+  menu.classList.toggle("fa-times"); // chuyern thành dấu x
+  navbar.classList.toggle("active"); //Thêm hoạt động active
+};
+
+//! Hoặc cách như thế này
+// ?function addEvent() {
+// ?  menu.classList.toggle("fa-times");
+// ?  navbar.classList.toggle("active");
+// ?}
+// ?menu.addEventListener("click", addEvent);
